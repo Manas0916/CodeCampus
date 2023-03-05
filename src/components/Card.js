c8041914147b76f6e1e0fc7520ab71164f20c82e
Abhay31 <abhaysharma0983@gmail.com>
2023-03-05 22:37:31 +0530

    Updated Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 //delete carditem and make changes in card.js and make folders of images named as banner and cards.
 import React from 'react';
-import one from './images/Cards/one.png'
-import two from './images/Cards/two.png'
-import three from './images/Cards/three.png'
-import four from './images/Cards/four.png'
-import five from './images/Cards/five.png'
-import six from './images/Cards/six.png'
+import { Link } from "react-router-dom";
+
+import projroad from './images/Cards/projroad.jpg'
+import sbp from './images/Cards/sbp.png'
+import pbp from './images/Cards/pbp.png'
+import cv from './images/Cards/cv.png'
+import cpdsa from './images/Cards/cpdsa.png'
+import aptitude from './images/Cards/aptitude.png'
+
 const Card = () => {
   return (
     <>
@@ -14,34 +17,58 @@ const Card = () => {
         <div className="row mx-3 my-5" style={{ display: 'flex', justifyContent: 'center' }}>
           <div className="col-md-3 mx-3">
             <div className="card" style={{ borderRadius: '20px' }}>
-              <a href="https://practice.geeksforgeeks.org/explore?page=1&category[]=Java&difficulty[]=0&sortBy=submissions&utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab" rel="noreferrer" target="_blank"><img src={one} style={{ borderRadius: '20px' }} className="card-img-top" alt="hello" /></a>
+              <img src={projroad} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
+              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
+                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Project Ideas and Roadmap</h1>
+                <Link to="/" class="btn btn-primary">Get Started</Link>
+              </div>
             </div>
           </div>
           <div className="col-md-3 mx-3">
             <div className="card" style={{ borderRadius: '20px' }}>
-              <a href="https://www.google.com/" rel="noreferrer" target="_blank"><img src={two} style={{ borderRadius: '20px' }} className="card-img-top" alt="hello" /></a>
+              <img src={sbp} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
+              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
+                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Service Based Preparation</h1>
+                <Link to="/" class="btn btn-primary">Get Started</Link>
+              </div>
             </div>
           </div>
           <div className="col-md-3 mx-3">
             <div className="card" style={{ borderRadius: '20px' }}>
-              <a href="https://www.google.com/" rel="noreferrer" target="_blank"><img src={three} style={{ borderRadius: '20px' }} className="card-img-top" alt="hello" /></a>
+              <img src={pbp} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
+              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
+                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Product Based Preparation</h1>
+                <Link to="/" class="btn btn-primary">Get Started</Link>
+              </div>
             </div>
           </div>
         </div>
         <div className="row mx-3 my-5" style={{ display: 'flex', justifyContent: 'center' }}>
-          <div className="col-md-3 mx-3">
+        <div className="col-md-3 mx-3">
             <div className="card" style={{ borderRadius: '20px' }}>
-              <a href="https://www.google.com/" rel="noreferrer" target="_blank"><img src={four} style={{ borderRadius: '20px' }} className="card-img-top" alt="hello" /></a>
+              <img src={cv} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
+              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
+                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Resume / CV Building</h1>
+                <Link to="/" class="btn btn-primary">Get Started</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 mx-3">
+            <div className="card" style={{ borderRadius: '20px', height: '530px' }}>
+              <img src={cpdsa} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
+              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px', height: '20px'}}>
+                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px', fontSize: '27px', height: '50px' }}>Competitive Programming / DSA Learning</h1>
+                <Link to="/" class="btn btn-primary">Get Started</Link>
+              </div>
             </div>
           </div>
           <div className="col-md-3 mx-3">
             <div className="card" style={{ borderRadius: '20px' }}>
-              <a href="https://www.google.com/" rel="noreferrer" target="_blank"><img src={five} style={{ borderRadius: '20px' }} className="card-img-top" alt="hello" /></a>
-            </div>
-          </div>
-          <div className="col-md-3 mx-3">
-            <div className="card" style={{ borderRadius: '20px' }}>
-              <a href="https://www.google.com/" rel="noreferrer" target="_blank"><img src={six} style={{ borderRadius: '20px' }} className="card-img-top" alt="hello" /></a>
+              <img src={aptitude} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
+              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
+                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Aptitude Preparation</h1>
+                <Link to="https://www.indiabix.com/" class="btn btn-primary">Get Started</Link>
+              </div>
             </div>
           </div>
         </div>
