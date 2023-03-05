892d01bb8b568b069b94097faefefb760ff4123c
Abhay31 <abhaysharma0983@gmail.com>
2023-03-06 00:30:17 +0530

    Updated Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-import projroad from './images/Cards/projroad.jpg'
-import sbp from './images/Cards/sbp.png'
-import pbp from './images/Cards/pbp.png'
-import cv from './images/Cards/cv.png'
-import cpdsa from './images/Cards/cpdsa.png'
-import aptitude from './images/Cards/aptitude.png'
+// import projroad from './images/Cards/projroad.jpg'
+// import sbp from './images/Cards/sbp.png'
+// import pbp from './images/Cards/pbp.png'
+// import cv from './images/Cards/cv.png'
+// import cpdsa from './images/Cards/cpdsa.png'
+// import aptitude from './images/Cards/aptitude.png'
 
 const Card = () => {
   return (
@@ -16,58 +16,70 @@ const Card = () => {
       <div className="conatiner my-3">
         <div className="row mx-3 my-5" style={{ display: 'flex', justifyContent: 'center' }}>
           <div className="col-md-3 mx-3">
-            <div className="card" style={{ borderRadius: '20px' }}>
-              <img src={projroad} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
-              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
-                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Project Ideas and Roadmap</h1>
-                <Link to="/" class="btn btn-primary">Get Started</Link>
+            <div className="card" style={{ width: '25rem', height: '18rem', backgroundColor: '#eeeeef' }}>
+              <div className="card-body">
+                <h4 className="card-title" sytle={{backgroundColor: '#74ad60'}}>Project Ideas</h4>
+                <p className="card-text my-5"><strong>Stuck in project ideas? Take help with some interesting ideas!</strong></p>
+                <div className="container my-5">
+                  <Link to="/" target="_blank" class="btn btn-outline-primary">Get Started</Link>
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-3 mx-3">
-            <div className="card" style={{ borderRadius: '20px' }}>
-              <img src={sbp} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
-              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
-                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Service Based Preparation</h1>
-                <Link to="/" class="btn btn-primary">Get Started</Link>
+            <div className="card" style={{ width: '25rem', height: '18rem', backgroundColor: '#eeeeef' }}>
+              <div className="card-body">
+                <h4 className="card-title" sytle={{backgroundColor: '#74ad60'}}>Service Based Preparation</h4>
+                <p className="card-text my-5"><strong>Stuck in project ideas? Take help with some interesting ideas!</strong></p>
+                <div className="container my-5">
+                  <Link to="/" target="_blank" class="btn btn-outline-primary">Get Started</Link>
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-3 mx-3">
-            <div className="card" style={{ borderRadius: '20px' }}>
-              <img src={pbp} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
-              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
-                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Product Based Preparation</h1>
-                <Link to="/" class="btn btn-primary">Get Started</Link>
+            <div className="card" style={{ width: '25rem', height: '18rem', backgroundColor: '#eeeeef' }}>
+              <div className="card-body">
+                <h4 className="card-title" sytle={{backgroundColor: '#74ad60'}}>Product Based Preparation</h4>
+                <p className="card-text my-5"><strong>Stuck in project ideas? Take help with some interesting ideas!</strong></p>
+                <div className="container my-5">
+                  <Link to="/" target="_blank" class="btn btn-outline-primary">Get Started</Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="row mx-3 my-5" style={{ display: 'flex', justifyContent: 'center' }}>
-        <div className="col-md-3 mx-3">
-            <div className="card" style={{ borderRadius: '20px' }}>
-              <img src={cv} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
-              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
-                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Resume / CV Building</h1>
-                <Link to="/" class="btn btn-primary">Get Started</Link>
+          <div className="col-md-3 mx-3">
+            <div className="card" style={{ width: '25rem', height: '18rem', backgroundColor: '#eeeeef' }}>
+              <div className="card-body">
+                <h4 className="card-title" sytle={{backgroundColor: '#74ad60'}}>Resume / CV Building</h4>
+                <p className="card-text my-5"><strong>Stuck in project ideas? Take help with some interesting ideas!</strong></p>
+                <div className="container my-5">
+                  <Link to="/" target="_blank" class="btn btn-outline-primary">Get Started</Link>
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-3 mx-3">
-            <div className="card" style={{ borderRadius: '20px', height: '530px' }}>
-              <img src={cpdsa} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
-              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px', height: '20px'}}>
-                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px', fontSize: '27px', height: '50px' }}>Competitive Programming / DSA Learning</h1>
-                <Link to="/" class="btn btn-primary">Get Started</Link>
+            <div className="card" style={{ width: '25rem', height: '18rem', backgroundColor: '#eeeeef' }}>
+              <div className="card-body">
+                <h4 className="card-title" sytle={{backgroundColor: '#74ad60'}}>Competitive Programming / DSA Learning</h4>
+                <p className="card-text my-5"><strong>Stuck in project ideas? Take help with some interesting ideas!</strong></p>
+                <div className="container my-5">
+                  <Link to="/" target="_blank" class="btn btn-outline-primary">Get Started</Link>
+                </div>
               </div>
             </div>
           </div>
           <div className="col-md-3 mx-3">
-            <div className="card" style={{ borderRadius: '20px' }}>
-              <img src={aptitude} style={{ height: '400px', borderRadius: '20px' }} className="card-img-top" alt="hello" />
-              <div class="card-body" style={{ backgroundColor: 'green', borderRadius: '20px' }}>
-                <h1 class="card-title" style={{ color: 'red', backgroundColor: 'yellow', borderRadius: '20px' }}>Aptitude Preparation</h1>
-                <Link to="https://www.indiabix.com/" class="btn btn-primary">Get Started</Link>
+            <div className="card" style={{ width: '25rem', height: '18rem', backgroundColor: '#eeeeef' }}>
+              <div className="card-body">
+                <h4 className="card-title">Aptitude Preparation</h4>
+                <p className="card-text my-5"><strong>Stuck in project ideas? Take help with some interesting ideas!</strong></p>
+                <div className="container my-5">
+                  <Link to="https://www.indiabix.com/" target="_blank" class="btn btn-outline-primary">Get Started</Link>
+                </div>
               </div>
             </div>
           </div>
