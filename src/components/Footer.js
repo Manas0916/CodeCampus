95c1b2950023dac0610f5eea10e94bd1e60d2bf8
Manas Bajpai <manas.bajpai16@gmail.com>
2023-03-25 17:36:08 +0530

    fixed some routes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,8 +23,8 @@ const Footer = () => {
                   <li className="list-group mb-2 mt-2" style={{ fontSize: '20px'}}><strong>Explore</strong></li>
                   <li className="list-group"><Link id="cs" className="link" to="/" onClick={()=>{window.scrollTo(0, 0);}}><b>Home</b></Link></li>
                   <li className="list-group"><Link id="cs" className="link" to="/about"><b>About</b></Link></li>
-                  <li className="list-group"><Link id="cs" className="link" to="/"><b>Service</b></Link></li>
-                  <li className="list-group"><Link id="cs" className="link" to="/"><b>Contact</b></Link></li>
+                  <li className="list-group"><Link id="cs" className="link" to="*"><b>Service</b></Link></li>
+                  <li className="list-group"><Link id="cs" className="link" to="*"><b>Contact</b></Link></li>
                 </ul>
               </div>
             </div>
