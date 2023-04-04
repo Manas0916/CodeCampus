405ed867f8168bd44760d0ef2559676ee9c1acfe
Abhay31 <abhaysharma0983@gmail.com>
2023-04-04 16:41:20 +0530

    Updated Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import {FcGoogle} from 'react-icons/fc';
-import {auth, provider} from './config';
-import { signInWithPopup} from 'firebase/auth';
+import { FcGoogle } from 'react-icons/fc';
+import { auth, provider } from './config';
+import { signInWithPopup } from 'firebase/auth';
 import Home from './Home';
+import login from "./images/Login/login.png";
 
 
 
-const Login = ({mode, toggleMode}) => {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
+const Login = ({ mode, toggleMode }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, []);
+  //   const [email, setEmail] = useState('');
+  //   const [password, setPassword] = useState('');
   const [value, setValue] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Add your login logic here, such as making an API call to authenticate the user
-    signInWithPopup(auth, provider).then((data)=>{
-        setValue(data.user.email);
-        localStorage.setItem('email', data.user.email);
+    signInWithPopup(auth, provider).then((data) => {
+      setValue(data.user.email);
+      localStorage.setItem('email', data.user.email);
     });
   };
 
@@ -27,25 +31,43 @@ const Login = ({mode, toggleMode}) => {
 
   return (
     <>
-    {value ? <Home mode={mode} toggleMode={toggleMode}/> :
-    <div className="container mt-5 p-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6 col-lg-4">
-          <div className="my-5 py-4 card shadow " style={{borderRadius: "11px"}}>
-            <div className="card-body p-5">
-              <h2 className="card-title text-center mb-4 text-muted">Login with Google</h2>
+      {value ? <Home mode={mode} toggleMode={toggleMode} /> :
+        <div className="container dd mt-5 p-5">
+          <div className="content text-start justify-content p-5">
+            <p style={{ fontSize: '60px', fontWeight: 'bold', color: mode === 'dark' ? 'white' : 'black' }}>Code Campus</p>
+            <p style={{ fontSize: '30px', fontWeight: 'bold', color: mode === 'dark' ? 'white' : 'black' }} className="mt-0">Code Helping Website</p>
+            <p style={{ fontSize: '20px', color: mode === 'dark' ? 'white' : 'black' }} className="mt-0">An online learning website dedicated to helping students learn how to code. Our mission is to provide a supportive and engaging learning environment that empowers students to achieve their goals and build a brighter future.</p>
+            <div>
               <form onSubmit={handleSubmit}>
-                <div className="text-center">
-                  <Link onClick={handleSubmit}><FcGoogle style={{fontSize: "40px"}}/></Link>
+                <div className="text-start">
+                  <Link onClick={handleSubmit}><FcGoogle style={{ fontSize: "50px" }} /></Link>
                 </div>
               </form>
             </div>
           </div>
+          <div className="content text-end justify-content p-5">
+            <img src={login} alt="Mac" />
+          </div>
         </div>
-      </div>
-    </div>
-  }
-  </>
+
+        // <div className="container mt-5 p-5">
+        //   <div className="row justify-content-center">
+        //     <div className="col-md-6 col-lg-4">
+        //       <div className="my-5 py-4 card shadow " style={{ borderRadius: "11px" }}>
+        //         <div className="card-body p-5">
+        //           <h2 className="card-title text-center mb-4 text-muted">Login with Google</h2>
+        //           <form onSubmit={handleSubmit}>
+        //             <div className="text-center">
+        //               <Link onClick={handleSubmit}><FcGoogle style={{ fontSize: "40px" }} /></Link>
+        //             </div>
+        //           </form>
+        //         </div>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
+      }
+    </>
   );
 };
 
